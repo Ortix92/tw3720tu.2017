7ed1cea1c948a8fcd888d8ddcb459cb65ba090ac
Ortix <nicktsutsunava@gmail.com>
2018-01-25 17:41:09 +0100

    Moved everything back to main for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
-#include <matrix.h>
+// #include <matrix.h>
+#include <array>
+#include <map>
 
+template <typename T>
+class Matrix
+{
+  size_t rows;
+  size_t cols;
+
+public:
+  using key_type = std::array<size_t, 2>;
+  using mapped_type = T;
+  using map_type = std::map<key_type, mapped_type>;
+  map_type data;
+
+  Matrix(size_t r, size_t c) : rows(r), cols(c) {}
+
+  const T &operator[](key_type key) const { return data[key]; }
+  T &operator[](key_type key) { return data[key]; }
+};
 int main(int, char **)
 {
 
--- src/matrix.cpp
+++
@@ -1,9 +0,0 @@
-#include <array>
-#include <map>
-
-template <typename T>
-class Matrix
-{
-
-  // other operations as appropriate
-};
